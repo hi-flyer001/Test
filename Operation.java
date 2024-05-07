50d3dab3ff2db823bc8acc8b19df68da4a58cbaa
hi-flyer001 <gaurav.borana2012@gmail.com>
2024-05-07 18:28:34 +0530

    Done bro

---
+++ Operation.java
@@ -0,0 +1,18 @@
+class Operation {
+
+	public void add(int a, int b) {
+
+		int c = a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String args[]) {
+		
+	
+	Operation o = new Operation();
+
+		o.add(10,20);
+
+	}
+
+}
